ecdd2b0445a94331fb75a0538799310183322efb
Tanvir Chahal <tnvr009@gmail.com>
2020-06-07 00:40:12 -0400

    Added resource metadata page

--- src/components/ResourceMetadata.js
+++ src/components/ResourceMetadata.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import WrappedField from "../WrappedField"
+import FieldInfo from "../FieldInfo"
+import Notification from "../Notification"
+import Checkbox from './Checkbox'
+import Radio from './Radio'
 
 export class ResourceMetadata extends Component {
     render() {
@@ -6,9 +11,64 @@ export class ResourceMetadata extends Component {
             return null
         }
         return (
-            <div>
-                <h1>Resource Metadata</h1>
-            </div>
+            <div className="usa-form-custom">
+                <section id="section-basic-mega-menu" className="site-component-section">
+                    <h1 className="usite-page-title" id="basic-mega-menu">Resource Upload</h1>
+                    <p className="site-text-intro">Introductory text if needed. Maecenas faucibus mollis interdum. Etiam porta sem malesuada magna mollis euismod. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+                </section>
+                <div className="row">
+                    <div className="col-md-12">
+                        <WrappedField
+                            label="Name"
+                            name="name"
+                            type="string"
+                            placeholder=""
+                            helptext=""
+                            value={this.props.values.name}
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <label className="usa-label">Data</label>
+                        <br/>
+                        <input type="file" hidden ref={this.inputReference} onChange={this.fileUploadInputChange} />
+                        <button className="usa-button" onClick={this.fileUploadAction}>
+                            Upload
+                        </button>
+
+                        <input type="file" hidden ref={this.inputReference} onChange={this.fileUploadInputChange} />
+                        <button className="usa-button" onClick={this.fileUploadAction}>
+                            Link
+                        </button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <WrappedField
+                            label="Description"
+                            name="resource_description"
+                            type="string"
+                            component="textarea"
+                            rows="6"
+                            helptext="You can use Markdown Formatting here."
+                            value={this.props.values.resource_description}
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <WrappedField
+                            label="Media Type"
+                            name="media_type"
+                            type="select"
+                            choices={["Type 1 ", "Type 2", "Type 3", "Type 4"]}
+                            required={true}
+                            className=""
+                        />
+                    </div>
+                </div>
+        </div>
         )
     }
 }
